f80cd1de02146e9621d170432dc02532bdb3ca6c
José Daniel Calle Briceño <j.daniel.c137@gmail.com>
2023-05-02 16:58:05 -0500

    fix: dart auto fix

--- lib/src/ire_client/ire_client_task.dart
+++ lib/src/ire_client/ire_client_task.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 
 import 'package:dio/dio.dart';
 import '../task.dart';
--- test/ire_common_test.dart
+++ test/ire_common_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ire_common/ire_common.dart';
 
 void main() {
   test('adds one to input values', () {
